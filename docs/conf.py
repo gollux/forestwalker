c79c06d817554e3d22feeda85425fd19dc5be5dc
Martin Mareš <mj@ucw.cz>
2026-02-15 00:57:05 +0100

    Docs: One more occurrence of the old name

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Tree Walker'
+project = 'The Forest Walker'
 copyright = '2026, Martin Mareš'
 author = 'Martin Mareš'
 
